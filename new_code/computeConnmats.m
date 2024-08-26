2e3eb87f6de11b95b5781fedcbff2756d236bebc
AJ Simon <24576893+asimon445@users.noreply.github.com>
2024-08-26 14:15:05 -0400

    Add files via upload

--- new_code/computeConnmats.m
+++ new_code/computeConnmats.m
@@ -6,18 +6,15 @@ clc; clear; close all;
 addpath(genpath('/home/ajs332/Documents/NIfTI_20140122/'));
 
 data_path = '/mnt/dustin/data/REST_LR/preprocessd_output/';
-save_path = '/data22/mri_group/dustinlab_data/dustinlab/Documents/AJ/JointParcelPredict_dev/';
+save_path = '/data22/mri_group/dustinlab_data/dustinlab/Documents/AJ/JointParcelPredict_dev/Voxel_connmats/';
 
 all_d = dir([data_path, '*_REST_LR_GSR_preprocessed_output.nii.gz']);
+subvols_path = '/data22/mri_group/dustinlab_data/dustinlab/Documents/AJ/JointParcelPredict_dev/Subvolumes_info/';
 
 no_sub = length(all_d);
 
-load('/data22/mri_group/dustinlab_data/dustinlab/Documents/AJ/JointParcelPredict_dev/JointParcelPredict-main/new_code/HCP_overlap_mask.mat');
-
-
-%% 
 ix=0;
-side = {'Right','Left'};
+side = {'R','L'};
 
 for f = 1:length(all_d)
 
@@ -30,29 +27,46 @@ for f = 1:length(all_d)
 
         img = data.img;
 
-        % populate this patient's image with only voxels in the masks
-        img_cleaned = zeros(size(img));
+        % Loop through both sides
+        for sd = 1:length(side)
+            subvols_path_temp = sprintf('%sSubvolumes_%s/',subvols_path,side{sd});
+            subvols_temp = dir([subvols_path_temp '*.mat']);
 
-        for v = 1:size(x_pos)
-            img_cleaned(x_pos(v),y_pos(v),z_pos(v),:) = img(x_pos(v),y_pos(v),z_pos(v),:);
+            % loop through each subvolume
+            for sv = 1:length(subvols_temp)
+
+                load([subvols_temp(sv).folder '/' subvols_temp(sv).name]);
+
+                img_sub = zeros(size(img));
+
+                for v = 1:length(subvol)
+                    img_sub(subvol(v,1),subvol(v,2),subvol(v,3),:) = img(subvol(v,1),subvol(v,2),subvol(v,3),:);
+                end
+
+                dvec = reshape(img_sub,[(size(img_sub,1)*size(img_sub,2)*size(img_sub,3)),size(img_sub,4)]);
+
+                %remove rows of zeros
+                dvec(~any(dvec,2),:) = [];
+                dvec = double(dvec);
+
+                connmat(:,:) = double(corr(dvec(:,:)',dvec(:,:)'));
+                connmat = normalize(connmat);
+
+                fnum = subvols_temp(sv).name(15:16);
+
+                outpath = sprintf('%s/Subvol_%s_%s',save_path,fnum,side{sd});  % ,side{s});
+
+                if ~exist("outpath",'dir')
+                    mkdir(outpath);
+                end
+
+                outfile = [outpath '/' all_d(f).name(1:11)];
+                save(outfile,'connmat','-v7.3');
+
+                clear dvec img_sub connmat outfile outpath fnum
+            end
+            clear subvols_path_temp subvols_temp
         end
-
-        dvec = reshape(img_cleaned,[(size(img_cleaned,1)*size(img_cleaned,2)*size(img_cleaned,3)),size(img_cleaned,4)]);
-
-        %remove rows of zeros
-        dvec(~any(dvec,2),:) = [];
-        dvec = double(dvec);
-
-        connmat(:,:) = double(corr(dvec(:,:)',dvec(:,:)'));
-        connmat = normalize(connmat);
-
-        outfile = sprintf('/data22/mri_group/dustinlab_data/dustinlab/Documents/AJ/JointParcelPredict_dev/compare_to_shen368/HCP_%s.mat',...
-            all_d(ix).name(1:11));  % ,side{s});
-
-        save(outfile,'connmat','-v7.3');
-        clear dvec img_cleaned connmat outfile % x_pos y_pos z_pos
-%         end
-        fprintf('%d ',ix);
     end
 end
 
